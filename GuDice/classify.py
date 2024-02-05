850ec5ad4237d89f915abc6f818816a4dcc8b416
白咕咕 <2432115441@qq.com>
2024-02-05 12:48:22 +0800

    更新分类器

--- GuDice/classify.py
+++ GuDice/classify.py
@@ -8,7 +8,10 @@ class Classify:
 
     def __init__(self, data: dict):
         self.data = data
+        self.bot_id = data["self_id"]
         print(data)
+
+        # 消息分类
         if data['post_type'] == "message":
             if data['message_type'] == "private":
                 self.private_message()
@@ -16,10 +19,32 @@ class Classify:
             elif data['message_type'] == "group":
                 self.group_message()
                 self.type = "group_message"
+        # 通知分类
         elif data['post_type'] == "notice":
-            if data['sub_type'] == "poke":
-                self.poke()
-                self.type = "poke"
+            if data['notice_type'] == "notify":
+                if data['sub_type'] == "poke":
+                    if 'group_id' in data:
+                        self.group_poke()
+                        self.type = "group_poke"
+                    else:
+                        self.private_poke()
+                        self.type = "private_poke"
+                elif data['sub_type'] == "sign":
+                    self.type = "group_sign"
+
+            elif data['notice_type'] == "group_admin":
+                self.type = "group_poke"
+            elif data['notice_type'] == "group_decrease":
+                if data['sub_type'] == "leave":
+                    self.type = "group_poke"
+
+        # 请求分类
+        elif data['post_type'] == "request":
+            if data['request_type'] == "friend":
+                self.type = "group_poke"
+
+        else:
+            print("未解析的事件")
 
     def private_message(self):
         data = MessageData()
@@ -42,8 +67,12 @@ class Classify:
         self.data = data
         print("群聊消息 QQ：{} 群昵称：{} 内容：{}".format(data.user_id, data.user_nick, data.message))
 
-    def poke(self):
-        data = NoticeData()
+    def private_poke(self):
+        pass
+
+    def group_poke(self):
+        pass
 
     def result(self):
-        return [self.type, self.data]
+        print([self.bot_id, self.type, self.data])
+        return [self.bot_id, self.type, self.data]
