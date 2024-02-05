7713b403ce7767268f1f2afbeffb63f4d00a2e8a
白咕咕 <2432115441@qq.com>
2024-02-05 12:49:27 +0800

    更新插件加载器

--- GuDice/plugin.py
+++ GuDice/plugin.py
@@ -1,10 +1,36 @@
-### 插件式框架
 import os
 import sys
-from imp import find_module
-from imp import load_module
+import importlib
+from os.path import abspath, dirname, join, exists
+
 
 class PluginManager:
+    plugin_dir = abspath('../plugin')
+    plugin_list = []
+
     def __init__(self):
+        if self.plugin_dir not in sys.path:
+            sys.path.append(self.plugin_dir)
+        self.test = None
+
+    def plugin_registered(self):
+        for item in os.scandir(self.plugin_dir):
+            if item.is_dir():
+                if (exists(join(self.plugin_dir, item.name, "config.toml"))
+                        and exists(join(self.plugin_dir, item.name, "main.py"))):
+                    self.test = importlib.import_module(item.name)
+                else:
+                    print(item.name + "缺失文件")
+
+    def plugin_run(self):
+
         pass
 
+    def plugin_test(self):
+        pass
+
+
+if __name__ == "__main__":
+    Manager = PluginManager()
+    Manager.plugin_registered()
+    print(Manager.test.data)
