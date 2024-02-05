51b7d0a10bbb1e92a75edcbef7b069a0c5b77628
白咕咕 <2432115441@qq.com>
2024-02-05 12:47:51 +0800

    添加插件管理器

--- GuDice/__init__.py
+++ GuDice/__init__.py
@@ -4,10 +4,11 @@ from GuDice.api import API
 from GuDice.classify import Classify
 from GuDice.dice import Dice
 from GuDice.event import Event
-
+from GuDice.plugin import PluginManager
 __all__ = [
     "API",
     "Classify",
     "Dice",
     "Event",
+    "PluginManager"
 ]
