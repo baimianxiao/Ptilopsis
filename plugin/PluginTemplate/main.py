1d682e36ecc2b3f3e7a7744b5a3d8ae039aca3f1
白咕咕 <2432115441@qq.com>
2024-02-12 20:07:25 +0800

    修改插件继承类型

--- plugin/PluginTemplate/main.py
+++ plugin/PluginTemplate/main.py
@@ -1,8 +1,8 @@
 # -*- encoding:utf-8 -*-
-from GuDice import Event
+from GuDice import Plugin
 
 
-class PluginEvent(Event):
+class PluginEvent(Plugin):
     def private_message(self, data, bot):
         if data.message == "咕":
             bot.send_private_msg(2432115441, "咕")
