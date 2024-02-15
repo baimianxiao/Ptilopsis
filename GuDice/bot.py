c8e38c7eb00346e82352c0e4fe09ab64e176de6f
白咕咕 <2432115441@qq.com>
2024-02-15 17:50:16 +0800

    实现reply方法

--- GuDice/bot.py
+++ GuDice/bot.py
@@ -1,2 +1,22 @@
 # -*- encoding:utf-8 -*-
+from GuDice import API
+import sqlite3
 
+
+class Bot(API):
+    bot_result = {}
+
+    def __init__(self, host: str, port: int):
+        super().__init__(host, port)
+
+    def reply(self, message):
+        self.bot_result = {
+            "reply": message,
+            "auto_escape": True,
+            "auto_reply": False
+        }
+
+    def result(self):
+        result = self.bot_result
+        self.bot_result = {}
+        return result
