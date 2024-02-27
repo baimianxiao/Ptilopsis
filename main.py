8900d84940ff997206e83d9e43c5867033f42dfd
白咕咕 <2432115441@qq.com>
2024-02-28 00:48:16 +0800

    加入图片接口

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # -*- encoding:utf-8 -*-
+import ctypes
 import os, sys
 import json
-from GuDice import Bot,Classify, Event, PluginManager
-from flask import Flask, request
+from os.path import join
+
+from GuDice import Bot, Classify, Event, PluginManager
+from flask import Flask, request, Response, make_response, send_file
 from gevent import pywsgi
 
 app = Flask(__name__)
+
+main_dir = os.getcwd()
 
 
 # 根目录
@@ -17,6 +22,16 @@ def bot():
     event = Classify(data_json).result()
     result = Manager.plugin_event(event, bot)
     return result
+
+
+@app.route("/data/<path:imageId>", methods=['POST', 'GET'])
+def plugin_image(imageId):
+    try:
+        response = make_response(send_file(join(main_dir,"data", imageId)))
+        response.headers["Content-Type"] = "image/jpeg"
+        return response
+    except BaseException as error:
+        return {"code": '503', "data": str(error), "message": "图片不存在"}
 
 
 def server_start(mode="", host="127.0.0.1", port=5900):
@@ -34,7 +49,9 @@ def server_start(mode="", host="127.0.0.1", port=5900):
 
 
 if __name__ == "__main__":
-    print("version:0.0.2(2)")
+    version = "version:0.0.6(6)"
+    print(version)
+    ctypes.windll.kernel32.SetConsoleTitleW("Ptilopsis " + version)
     Manager = PluginManager()
     Manager.plugin_registered()
     bot = Bot("127.0.0.1", 5700)
